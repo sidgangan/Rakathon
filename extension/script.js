1827ade1500a0d10f4239f96fa5c712d9a5735d9
sidgangan <sgangan298@gmail.com>
2019-11-10 01:39:12 +0530

    loading gif added

--- extension/script.js
+++ extension/script.js
@@ -11,9 +11,12 @@ window.onload =  function(e){
             btn.setAttribute("class","btn btn-danger try-button");
             btn.setAttribute("data-toggle","modal");
             btn.setAttribute("data-target","#tryWindow");
+            btn.setAttribute("style","")
 
             btn.addEventListener('click',function(){
                 console.log("button clicked");
+                var loading = document.getElementById('loading');
+                loading.setAttribute("style","display:none");
             })
 
             var modalhtml = `<div class="modal fade" id="tryWindow">
@@ -21,24 +24,47 @@ window.onload =  function(e){
               <div class="modal-content">
               
                 <!-- Modal Header -->
-                <div class="modal-header">
-                  <h4 class="modal-title">Upload Your Image</h4>
-                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <div class="modal-header" style="text-align:center;">
+                  <h4 class="modal-title" style="margin: auto; display:block;">How it looks on you !</h4>
                 </div>
                 
                 <!-- Modal body -->
                 <div class="modal-body">
-                    <input type="file" id="fileinput" />
-                    </br>
-                    <img id="myImg" src="#" alt="your image"  width=100 />
-                    <div>
-                        <img id="myImg1" src="#" alt="your image" width=100 />
+                    <div  style="text-align:center;border-radius:20%; margin-bottom:20px;">
+                        <label for="fileinput" class="custom-file-upload">
+                            <i class="fa fa-cloud-upload"></i> Upload Your Image
+                        </label>
+                        <input id="fileinput" type="file" style="display:none;"/>
+                    </div>
+                    
+                    <div class="row content">
+                        <div class="overlay" id="loading"><div class="overlay-content"><img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/04de2e31234507.564a1d23645bf.gif" alt="Loading..."/></div></div>
+                        <div class="col-md-5">
+                        <center>
+                            <img id="user-img" src="#" alt="your image"  height="238" width="184" style="display:none;" />
+                        </center>
+                        </div>
+
+                        <div class="col-md-2" style="padding-top:18%">
+                        
+                        <center>
+                            <img id="plus" src="#" alt="your image"  height="64" width="64" style="display:none;" />
+                        </center>
+
+                        </div>
+
+                        <div class="col-md-5">
+                        <center>
+                            <img id="clothing-img" src="#" alt="your image"  height="238" width="184" style="display:none;" />
+                        </center>
+                        </div>
                     </div>
                 </div>
                 
                 <!-- Modal footer -->
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-danger" id="tryme" data-dismiss="modal">See how you look</button>
+                <div class="modal-footer" style="text-align:center">
+                  <button type="button" class="btn btn-danger" id="tryme" style="margin:auto; display:block;">Try the clothes !</button>
+                
                 </div>
                 
               </div>
@@ -48,13 +74,23 @@ window.onload =  function(e){
             var modal = document.createElement("div");
             modal.innerHTML = modalhtml;
             target[0].appendChild(btn);
-            target[0].appendChild(modal)
+            target[0].appendChild(modal);
 
             function imageIsLoaded(e) {
                  user_image = e.target.result;
                  clothing_image = document.getElementsByClassName("pointer pimg")[0].src;
-                 $('#myImg').attr('src',user_image);
-                 $('#myImg1').attr('src', clothing_image);
+                 $('#user-img').attr('src',user_image);
+                 $('#user-img').attr('style','display:true');
+
+                 $('#clothing-img').attr('src',clothing_image);
+                 $('#clothing-img').attr('style','display:true');
+
+                 $('#plus').attr('src',"https://image.flaticon.com/icons/svg/61/61112.svg");
+                 $('#plus').attr('style','display:true;');
+
+                 var loading = document.getElementById('loading');
+                 loading.setAttribute("style","display:true");
+                
 
             };
             
@@ -82,6 +118,30 @@ window.onload =  function(e){
             }, false);
 
             document.getElementById("tryme").addEventListener('click',function(e){
+
+                // var form = new FormData();
+                // form.append("person", user_image);
+                // form.append("clothing", clothing_image);
+
+                // var settings = {
+                // "async": true,
+                // "crossDomain": true,
+                // "url": "http://10.99.7.158:5000/api/infer",
+                // "method": "POST",
+                // "headers": {
+                //     "cache-control": "no-cache",
+                //     "postman-token": "6bb74c4e-86da-7b14-e2f2-3b306e02aa10"
+                // },
+                // "processData": false,
+                // "mimeType": false,
+                // "contentType": "multipart/form-data",
+                // "data": form
+                // }
+
+                // $.ajax(settings).done(function (response) {
+                // console.log(response);
+                // });
+
                 
             });
 
